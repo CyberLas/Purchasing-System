693b32c4e437d138c9922d9b2b89ec292a73c896
Carlos Angeles <33170529+CyberLas@users.noreply.github.com>
2020-08-17 17:52:36 -0500

    System Buy

---
+++ SYSTEM BUY/functions.php
@@ -0,0 +1,10 @@
+<?php 
+	date_default_timezone_set('America/Peru'); 
+	
+	function fechaC(){
+		$mes = array("","Enero", "Febrero",  "Marzo", "Abril", "Mayo",  "Junio", "Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+		return date('d')." de ". $mes[date('n')] . " de " . date('Y');
+	}
+
+
+ ?>
---
+++ SYSTEM BUY/index.php
@@ -0,0 +1,66 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>System Buy</title>
+</head>
+<body>
+	<header>
+		<div class="header">
+			
+			<h1>Sistema Facturación</h1>
+			<div class="optionsBar">
+				<p>Perú - Cajamarca, 02 octubre de 2019</p>
+				<span>|</span>
+				<span class="user">Carlos Angeles</span>
+				<img class="photouser" src="img/user.png" alt="Usuario">
+				<a href="#"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
+			</div>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li class="principal">
+					<a href="#">Usuarios</a>
+					<ul>
+						<li><a href="#">Nuevo Usuario</a></li>
+						<li><a href="#">Lista de Usuarios</a></li>
+					</ul>
+				</li>
+				<li class="principal">
+					<a href="#">Clientes</a>
+					<ul>
+						<li><a href="#">Nuevo Cliente</a></li>
+						<li><a href="#">Lista de Clientes</a></li>
+					</ul>
+				</li>
+				<li class="principal">
+					<a href="#">Proveedores</a>
+					<ul>
+						<li><a href="#">Nuevo Proveedor</a></li>
+						<li><a href="#">Lista de Proveedores</a></li>
+					</ul>
+				</li>
+				<li class="principal">
+					<a href="#">Productos</a>
+					<ul>
+						<li><a href="#">Nuevo Producto</a></li>
+						<li><a href="#">Lista de Productos</a></li>
+					</ul>
+				</li>
+				<li class="principal">
+					<a href="#">Facturas</a>
+					<ul>
+						<li><a href="#">Nuevo Factura</a></li>
+						<li><a href="#">Facturas</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<section id="container">
+		<h1>Bienvenido al sistema</h1>
+	</section>
+</body>
+</html>
